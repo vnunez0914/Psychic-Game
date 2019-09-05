032ca6da3dc40e21249b4032c66ff78199a2acc5
Victor Nunez <vnunez0914@gmail.com>
2019-09-04 21:13:47 -0500

    built index with p id's and pseudo code js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+        //Pseudo Code
+//1. WHat will I need to make this game?
+//I will need an alphabet array, variable 
+//I will need a 'for'loop??
+// I will need if/else/else if statements for right and wrong guesses??
+//I will need to use document.getElementById().innerHTML
+
+//2. How is this game played?
+//  Player loses a guess each time the wrong letter is pressed.
+//  Player wins when the correct letter is pressed
+//  The game resets after player makes the correct guess
+
+//3. What will the computer do when player presses the correct letter?
+//  When the wrong letter is guessed the remaining Guesses decrease by 1 from 9 until 0 guesses remain, game over.
+//If the right letter is pressed the Wins increase by one
+//If all 9 guesses are used, the Loss increases by 1.
